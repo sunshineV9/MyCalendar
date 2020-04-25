643f62f18164ed19883460e73bd878e209274585
sunshineV9 <michael-gintenreiter@gmx.at>
2020-04-25 12:11:31 +0200

    Fixed style issues in ViewModelBase class;

--- MyCalendar/MyCalendar.Mobile/MyCalendar.Mobile/ViewModels/ViewModelBase.cs
+++ MyCalendar/MyCalendar.Mobile/MyCalendar.Mobile/ViewModels/ViewModelBase.cs
@@ -1,22 +1,15 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
+﻿using Prism.Mvvm;
 using Prism.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MyCalendar.Mobile.ViewModels
 {
     public class ViewModelBase : BindableBase, IInitialize, INavigationAware, IDestructible
     {
-        protected INavigationService NavigationService { get; private set; }
-
         private string _title;
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
+
+        protected INavigationService NavigationService { get; }
+
+        public string Title { get => _title; set => SetProperty(ref _title, value); }
 
         public ViewModelBase(INavigationService navigationService)
         {
@@ -25,22 +18,18 @@ namespace MyCalendar.Mobile.ViewModels
 
         public virtual void Initialize(INavigationParameters parameters)
         {
-
         }
 
         public virtual void OnNavigatedFrom(INavigationParameters parameters)
         {
-
         }
 
         public virtual void OnNavigatedTo(INavigationParameters parameters)
         {
-
         }
 
         public virtual void Destroy()
         {
-
         }
     }
 }
