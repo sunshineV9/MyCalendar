c1b1add3d1c8e2a8df2e75b783e1c17a519672aa
sunshineV9 <michael-gintenreiter@gmx.at>
2020-04-26 10:04:14 +0200

    Added NoShiftEffect to HomePage;

---
+++ MyCalendar/MyCalendar.Mobile/MyCalendar.Mobile.Android/Effects/NoShiftEffect.cs
@@ -0,0 +1,30 @@
+﻿using Android.Support.Design.BottomNavigation;
+using Android.Support.Design.Widget;
+using Android.Views;
+using MyCalendar.Mobile.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("MyCalendar")]
+[assembly: ExportEffect(typeof(NoShiftEffect), "NoShiftEffect")]
+namespace MyCalendar.Mobile.Droid.Effects
+{
+    public class NoShiftEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            if (!(Container.GetChildAt(0) is ViewGroup layout))
+                return;
+
+            if (!(layout.GetChildAt(1) is BottomNavigationView bottomNavigationView))
+                return;
+
+            // This is what we set to adjust if the shifting happens
+            bottomNavigationView.LabelVisibilityMode = LabelVisibilityMode.LabelVisibilityUnlabeled;
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
