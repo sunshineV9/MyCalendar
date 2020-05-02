ba3eec0a3fdadd8d57b6304ca227938e8a0a7d42
sunshineV9 <michael-gintenreiter@gmx.at>
2020-05-02 13:23:14 +0200

    Implemented IdentityModel.OidcClient for authorization flow;

---
+++ MyCalendar/MyCalendar.Mobile/MyCalendar.Mobile/Common/Services/Browser.cs
@@ -0,0 +1,41 @@
+﻿using IdentityModel.OidcClient.Browser;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace MyCalendar.Mobile.Common.Services
+{
+    public class Browser : IBrowser
+    {
+        public async Task<BrowserResult> InvokeAsync(BrowserOptions options, CancellationToken cancellationToken = default)
+        {
+            WebAuthenticatorResult authResult = await WebAuthenticator.AuthenticateAsync(new Uri(options.StartUrl), new Uri("com.companyname.mycalendar://login"));
+            return new BrowserResult()
+            {
+                Response = ParseAuthenticatorResult(authResult)
+            };
+        }
+
+        string ParseAuthenticatorResult(WebAuthenticatorResult result)
+        {
+            bool first = true;
+            var resultString = "com.companyname.mycalendar://login";
+
+            foreach (var property in result.Properties)
+            {
+                if (first)
+                {
+                    resultString += $"#{property.Key}={property.Value}";
+                    first = false;
+                }
+                else
+                {
+                    resultString += $"&{property.Key}={property.Value}";
+                }
+            }
+
+            return resultString;
+        }
+    }
+}
